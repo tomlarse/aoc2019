93f4714c0afe96397d6262b38c2395ede7ac2f8d
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2019-12-02 18:45:42 +0100

    Day1 Part1 solved

--- day1/day1.ps1
+++ day1/day1.ps1
@@ -1,9 +1,10 @@
-function Invoke-Day1 {
+function Invoke-Day1Part1 {
     [CmdletBinding()]
     param (
+        [parameter(Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
         [int]$Mass
     )
-    
-    [int]($Mass/3)-2
-
+    Process {
+        [Math]::Floor($Mass/3) - 2
+    }
 }
--- day1/tests/day1.tests.ps1
+++ day1/tests/day1.tests.ps1
@@ -1,15 +1,17 @@
 Describe "Day 1" {
     . .\day1\day1.ps1
     It "Mass of 12 should require 2 fuel" {
-        Invoke-Day1 -Mass 12 | Should Be 2
+        Invoke-Day1Part1 -Mass 12 | Should Be 2
     }
     It "Mass of 14 should require 2 fuel" {
-        Invoke-Day1 -Mass 14 | Should Be 2
+        Invoke-Day1Part1 -Mass 14 | Should Be 2
     }
     It "Mass of 1969 should require 654 fuel" {
-        Invoke-Day1 -Mass 1969 | Should Be 654
+        Invoke-Day1Part1 -Mass 1969 | Should Be 654
     }
     It "Mass of 100756 should require 33583 fuel" {
-        Invoke-Day1 -Mass 100756 | Should Be 33583
+        Invoke-Day1Part1 -Mass 100756 | Should Be 33583
     }
 }
+
+# Solve Part 1 Import-Csv .\day1\input1.csv -Header Mass | Invoke-Day1 | Measure-Object -Sum
