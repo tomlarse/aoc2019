ce92236a29bfe921e9c9bbd6ae8e2d8ff65e13dc
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2019-12-03 20:14:58 +0100

    Part 1 solved. Wildly ineffective, but solved!

--- day3/day3.ps1
+++ day3/day3.ps1
@@ -11,31 +11,9 @@ function Get-WirePathLines {
     foreach ($Line in $WirePath) {
         $Points = [System.Collections.ArrayList]::new()
         switch ($Line[0]) {
-            "L" {
-                $Direction = 'H'
-                for ($initialy = $y; $y -gt $initialy - [int]$Line.Substring(1); $y--) {
-                    $null = $Points.Add(
-                        [pscustomobject]@{
-                            x = $x
-                            y = $y
-                        }
-                    )
-                }
-            }
-            "R" {
-                $Direction = 'H'
-                for ($initialy = $y; $y -lt $initialy + [int]$Line.Substring(1); $y++) {
-                    $null = $Points.Add(
-                        [pscustomobject]@{
-                            x = $x
-                            y = $y
-                        }
-                    )
-                }
-            }
             "D" {
                 $Direction = 'V'
-                for ($initialx = $x; $x -gt $initialx - [int]$Line.Substring(1); $x--) {
+                for ($initialy = $y; $y -gt $initialy - [int]$Line.Substring(1); $y--) {
                     $null = $Points.Add(
                         [pscustomobject]@{
                             x = $x
@@ -46,6 +24,28 @@ function Get-WirePathLines {
             }
             "U" {
                 $Direction = 'V'
+                for ($initialy = $y; $y -lt $initialy + [int]$Line.Substring(1); $y++) {
+                    $null = $Points.Add(
+                        [pscustomobject]@{
+                            x = $x
+                            y = $y
+                        }
+                    )
+                }
+            }
+            "L" {
+                $Direction = 'H'
+                for ($initialx = $x; $x -gt $initialx - [int]$Line.Substring(1); $x--) {
+                    $null = $Points.Add(
+                        [pscustomobject]@{
+                            x = $x
+                            y = $y
+                        }
+                    )
+                }
+            }
+            "R" {
+                $Direction = 'H'
                 for ($initialx = $x; $x -lt $initialx + [int]$Line.Substring(1); $x++) {
                     $null = $Points.Add(
                         [pscustomobject]@{
@@ -64,4 +64,63 @@ function Get-WirePathLines {
         $null = $WirePathLines.Add($WirePathLine)
     }
     $WirePathLines
+}
+
+function Get-ManhattanDistance {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true)]
+        [int]$x,
+        [parameter(Mandatory = $true, Position = 1, ValueFromPipelineByPropertyName = $true)]
+        [int]$y
+    )
+    
+    [Math]::Abs($x) + [Math]::Abs($y)
+}
+
+function Invoke-Day3Part1 {
+    [CmdletBinding()]
+    param (
+        $WirePaths
+    )
+
+    $Wire1Lines = Get-WirePathLines -WirePath $WirePaths[0]
+    $Wire2Lines = Get-WirePathLines -WirePath $WirePaths[1]
+
+    $crosses = [system.collections.arraylist]::new()
+
+    foreach ($Vcoord in $($Wire1Lines | Where-Object Direction -eq "V").Points) {
+        foreach ($Hcoord in $($Wire2Lines | Where-Object Direction -eq "H").Points) {
+            if (
+                
+            ) {
+                if ($Hcoord.x -ne 0 -and $Hcoord.y -ne 0) {
+                    $null = $crosses.Add($Hcoord)
+                }
+            }
+        }
+    }
+
+    foreach ($Vcoord in $($Wire2Lines | Where-Object Direction -eq "V").Points) {
+        foreach ($Hcoord in $($Wire1Lines | Where-Object Direction -eq "H").Points) {
+            if ($Vcoord.x -eq $Hcoord.x -and $Vcoord.y -eq $Hcoord.y) {
+                if ($Hcoord.x -ne 0 -and $Hcoord.y -ne 0) {
+                    $null = $crosses.Add($Hcoord)
+                }
+            }
+        }
+    }
+
+    $ShortestManhattanDistance = 0
+
+    foreach ($cross in $crosses) {
+        if ($ShortestManhattanDistance -eq 0) {
+            $ShortestManhattanDistance = ($cross | Get-ManhattanDistance)
+        } else {
+            if (($cross | Get-ManhattanDistance) -lt $ShortestManhattanDistance) {
+                $ShortestManhattanDistance = ($cross | Get-ManhattanDistance)
+            }
+        }
+    }
+    $ShortestManhattanDistance
 }
--- day3/tests/day3.tests.ps1
+++ day3/tests/day3.tests.ps1
@@ -5,5 +5,11 @@ describe "Day 3 Tests" {
         It "Finds the closest crossing point for Example 1"  {
             Invoke-Day3Part1 -WirePaths @(@("U7","R6","D4","L4"),@("R8","U5","L5","D3")) | Should -Be 6
         }
+        It "Finds the closest crossing point for Example 2" {
+            Invoke-Day3Part1 -WirePaths @(@("R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"), @("U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83")) | Should -Be 159
+        }
+        It "Finds the closest crossing point for Example 3" {
+            Invoke-Day3Part1 -WirePaths @(@("R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"), @("U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7")) | Should -Be 135
+        }
     }
 }
