9fd8e4c59fdbdcb1d3f62f455d0fff24eef3348e
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2019-12-04 16:02:39 +0100

    I fell I've solved it, but I'm not getting the right answer... :/

--- day4/day4.ps1
+++ day4/day4.ps1
@@ -27,7 +27,42 @@ function Test-Password {
     }
 }
 
+function Test-Password2 {
+    [CmdletBinding()]
+    param (
+        [ValidateLength(6, 6)]
+        [parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [string]$PassToTest
+    )
+
+    process {
+        $lastnum = $null
+        $TwoAdjacent = $false
+        $TwoAdjacentInt = $null
+        for ($i = 0; $i -lt 6; $i++) {
+            if ($null -ne $lastnum) {
+                if ($PassToTest[$i] -lt $lastnum) {
+                    return $false
+                }
+                if ($PassToTest[$i] -eq $lastnum -and $PassToTest[$i] -ne $TwoAdjacentInt) {
+                    $TwoAdjacent = $true
+                    $TwoAdjacentInt = $PassToTest[$i]
+                } elseif ($PassToTest[$i] -eq $lastnum -and $PassToTest[$i] -eq $TwoAdjacentInt) {
+                    $TwoAdjacent = $false
+                }
+            }
+            $lastnum = $PassToTest[$i]
+        }
+        if ($TwoAdjacent) {
+            $true
+        }
+        else {
+            $false
+        }
+    }
+}
+
 # Solve part 1
-# $t=0
-# 134792..675810 |% {if (Test-Password $_) {$t++}}
-# $t
+# 134792..675810 | Test-Password | ? {$_} | measure
+# Solve part 2
+# 134792..675810 | Test-Password2 | ? {$_} | measure
--- day4/tests/day4.tests.ps1
+++ day4/tests/day4.tests.ps1
@@ -12,4 +12,28 @@ Describe "Day 4 Tests" {
             Test-Password 123789 | Should -BeFalse
         }
     }
+
+    Context "Part 2" {
+        It "Judges passwords correctly example 1" {
+            Test-Password2 112233 | Should -BeTrue
+        }
+        It "Judges passwords correctly example 2" {
+            Test-Password2 123444 | Should -BeFalse
+        }
+        It "Judges passwords correctly example 3" {
+            Test-Password2 111122 | Should -BeTrue
+        }
+        It "Judges passwords correctly self" {
+            Test-Password2 111111 | Should -BeFalse
+        }
+        It "Judges passwords correctly self 2" {
+            Test-Password2 122234 | Should -BeFalse
+        }
+        It "Judges passwords correctly self 3" {
+            Test-Password2 122255 | Should -BeTrue
+        }
+        It "Judges passwords correctly self 4" {
+            Test-Password2 111145 | Should -BeFalse
+        }
+    }
 }
